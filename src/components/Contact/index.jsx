b7c904c58673c4af616c00cdca78b4cc7dc8772e
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-07 21:39:26 -0300

    add portfolio

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,12 +2,19 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import React from 'react'
 import { Loader } from 'react-loaders'
-import emailjs from '@emailjs/browser'
+// import emailjs from '@emailjs/browser'
+
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
 
     const [ letterClass, setLetterClass ] = React.useState('text-animate')
-    const refForm = React.useRef()
+    // const refForm = React.useRef()
+
+    const [state, handleSubmit] = useForm("moqzqnvj");
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
 
     React.useEffect(() => {
         
@@ -20,16 +27,30 @@ const Contact = () => {
                 }
     }, [])
 
+    // const sendEmail = (event) => {
+    //     event.preventDefault()
+
+    //     emailjs
+    //         .sendForm(
+    //             'gmail',
+    //             'template_asrux2i',
+    //             refForm.current,
+    //             'L8ThqCCedW8eX6iTuMEo9'
+    //         )
+    //         .then(
+    //             () => {
+    //                 alert('Message succesfully sent!')
+    //                 window.location.reload(false)
+    //             },
+    //             () => {
+    //                 alert('Failed to send the message, please try again!')
+    //             }
+    //         )
+    // }
+
     const sendEmail = (event) => {
         event.preventDefault()
-
-        emailjs
-            .sendForm(
-                'gmail',
-                'template_asrux2i',
-                refForm.current,
-                
-            )
+        handleSubmit()
     }
 
     return(
@@ -47,7 +68,36 @@ const Contact = () => {
                     I'm excited about the opportunity to collaborate with you on exciting and challenging web projects. If you're looking for a passionate and dedicated web developer to bring your online vision to life, please don't hesitate to contact me. I'm here to listen to your ideas, bring my expertise to the table, and work together to create exceptional web solutions. I look forward to meeting you and getting started on your next project!
                     </p>
                     <div className='contact-form'>
-                        <form ref={refForm} onSubmit={sendEmail}>
+                        <form onSubmit={sendEmail}>
+                            <ul>
+                                <li className='half'>
+                                    <input type="text" name='name' placeholder='Name' required/>
+                                </li>
+                                <li className='half'>
+                                    <input type="email" name='email' placeholder='Email' required/>
+                                    <ValidationError 
+                                        prefix="Email" 
+                                        field="email"
+                                        errors={state.errors}
+                                    />
+                                </li>
+                                <li>
+                                    <input placeholder='Subject' type='text' name="subject" required/>
+                                </li>
+                                <li>
+                                    <textarea placeholder='Message' name='message' required></textarea>
+                                    <ValidationError 
+                                        prefix="Message" 
+                                        field="message"
+                                        errors={state.errors}
+                                    />
+                                </li>
+                                <li>
+                                    <input type="submit" className='flat-button' disabled={state.submitting} value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                        {/* <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input type="text" name='name' placeholder='Name' required/>
@@ -65,7 +115,7 @@ const Contact = () => {
                                     <input type="submit" className='flat-button' value="SEND" />
                                 </li>
                             </ul>
-                        </form>
+                        </form> */}
                     </div>
                 </div>
             </div>
